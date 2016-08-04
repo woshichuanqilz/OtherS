2cb39e3ed28c0ba7d9389e579fc225039d9a1976
zhe li <zhe li>
2016-08-04 10:04:47 +0800

    add extractfile

---
+++ bash/extractfile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# mkdir of the directory
+mkdir -p logfolder
+
+# define the extension of the file
+ext="_log.logr"
+folder="./logfolder/"
+
+
+awk  '{ a[$7]++ } END { for (b in a) { print b } }' *.log > UserID.txt
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    filename="$folder$line$ext"
+    echo "file name is: $filename"
+    grep -E $line *.log  > $filename
+done < UserID.txt
